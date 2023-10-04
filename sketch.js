099b5013abeb7cf2a7c69a81e84481f645ab7767
Ashley Harrop <144391801+ashleyharrop@users.noreply.github.com>
2023-10-04 19:43:11 -0400

    Co-authored-by: Rob Duarte <me@robduarte.com>

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
+
+let img;
+let matchIsBeingDragged = false;
+
 function setup() {
-  createCanvas(400, 400);
+  noCanvas();
+  
+  let p = createP("FLASHLIGHT");
+  
+  let button = createButton("1");
+  button.mouseClicked( button1click );
+
+  button = createButton("2");
+  button = createButton("3");
+  
+  img = createImg("assets/match.png");
+  img.mousePressed( matchPressed );
+  img.mouseReleased( matchReleased );
+  // img.style("width","40px");
+
+}
+
+function matchPressed() {
+  matchIsBeingDragged = true;
+}
+function matchReleased() {
+  matchIsBeingDragged = false;
 }
 
 function draw() {
-  background(125);
+  if (matchIsBeingDragged) img.position(mouseX-100,mouseY-100);
 }
+
+function button1click() {
+  console.log("clicked button 1");
+  location.href = "index2.html";
+}
+
+
+
