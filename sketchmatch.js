b1fed36a0bcef7e8b9df10507c23cdf87d30488e
Ashley Harrop <144391801+ashleyharrop@users.noreply.github.com>
2023-10-04 20:13:04 -0400

    all 3 buttons work

---
+++ sketchmatch.js
@@ -0,0 +1,13 @@
+function setup() {
+  noCanvas();
+
+// let img = loadImage(assets/matchstick.jpeg);
+// img.loadPixels();
+
+  button = createButton("Click Here");
+  button.position(CENTER);
+}
+
+function draw() {
+  background(125);
+}
