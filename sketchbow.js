000ef1cff2307781e28cf2e043d91719a2a5f564
Ashley Harrop <144391801+ashleyharrop@users.noreply.github.com>
2023-10-04 20:26:55 -0400

    Menu buttons working

--- sketchbow.js
+++ sketchbow.js
@@ -4,10 +4,18 @@ function setup() {
 // let img = loadImage(assets/matchstick.jpeg);
 // img.loadPixels();
 
-  button = createButton("Click Here");
-  button.position(CENTER);
+
+button = createButton("Menu");
+button.position(CENTER);
+button.mouseClicked(buttonMenuclicked);
+
 }
 
 function draw() {
   background(125);
 }
+
+function buttonMenuclicked () {
+  console.log("cliked menu button");
+  location.href = "index.html";
+ }
--- sketchmatch.js
+++ sketchmatch.js
@@ -4,10 +4,16 @@ function setup() {
 // let img = loadImage(assets/matchstick.jpeg);
 // img.loadPixels();
 
-  button = createButton("Click Here");
+  button = createButton("Menu");
   button.position(CENTER);
+  button.mouseClicked(buttonMenuclicked);
 }
 
 function draw() {
   background(125);
 }
+
+ function buttonMenuclicked () {
+  console.log("cliked menu button");
+  location.href = "index.html";
+ }
